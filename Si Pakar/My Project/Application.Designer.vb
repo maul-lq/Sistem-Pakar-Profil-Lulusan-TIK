e06db2ce9375e297fa71b7a1fe8b7e4968df69e8
Rizlrad Fz <rizlradfz@gmail.com>
2025-12-13 22:41:32 +0700

    Change main form from FHasil to FStart

--- Si Pakar/My Project/Application.Designer.vb
+++ Si Pakar/My Project/Application.Designer.vb
@@ -28,7 +28,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = FHasil
+            Me.MainForm = FStart
         End Sub
     End Class
 End Namespace
